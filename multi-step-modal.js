dc45596a1d50e24ab0a01e2e840ae3be568b2e60
Moisés Querol Ferrer <mquerol@culturaweb.com>
2015-06-29 21:45:13 +0200

    use modal-body for count steps instead of buttons

--- multi-step-modal.js
+++ multi-step-modal.js
@@ -5,8 +5,8 @@
 
     modals.each(function(idx, modal) {
         var $modal = $(modal);
-        var $buttons = $modal.find('button.step');
-        var total_num_steps = $buttons.length;
+        var $bodies = $modal.find('div.modal-body');
+        var total_num_steps = $bodies.length;
         var $progress = $modal.find('.m-progress');
         var $progress_bar = $modal.find('.m-progress-bar');
         var $progress_stats = $modal.find('.m-progress-stats');
@@ -105,7 +105,7 @@
             $progress_total.text(total_num_steps);
             bindEventsToModal($modal, total_num_steps);
             $modal.data({
-                total_num_steps: $buttons.length,
+                total_num_steps: $bodies.length,
             });
         }
 
